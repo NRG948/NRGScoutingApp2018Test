08ae46f46c65367b607aea9b56925e505ca0a93c
Shrey Aeron <shreystechtips@gmail.com>
2018-10-11 21:38:30 -0700

    Stores Match Events into a Dictionary
    Now takes the arrays that have been formed and adds them to a dictionary, where they can be accessed by their index (perhaps by using a for loop to check match event numbers in the dictionary and figure out time deltas for points/ranking)
    
    EXAMPLE PRINT TO CONSOLE IN DEBUG MODE (prints what is added to the dictionary):
    cubePicked0
    647
    cubeDroppedScale0
    1552
    cubePicked1
    3063
    cubeDroppedOppSwitch1
    3872
    cubePicked2
    6810
    cubeDroppedAllySwitch2
    7862
    cubePicked3
    9153
    cubeDroppedScale3
    9943
    cubePicked4
    11010
    climbStart
    14619

--- NRGScoutingApp/NRGScoutingApp/MatchEventsFormat.cs
+++ NRGScoutingApp/NRGScoutingApp/MatchEventsFormat.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Tracing;
 using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 
 namespace NRGScoutingApp
 {
@@ -8,24 +9,48 @@ namespace NRGScoutingApp
     // FORMAT OF EVENTS: (cubePicked:1000|cubeDroppedLocation0:5000||cubePicked:5500|cubeDropped:back||climbStart:139000||)
     public class MatchEventsFormat
     {
-        public MatchEventsFormat()
-        {
-        }
+        public MatchEventsFormat(){}
         public static Array ParseMatchEvents(string events)
         {
             events = events.Remove(0, 1);
             events = events.Remove(events.Length - 1, 1);
-            String[] sep = new String[] { "||" }; //fix sep with tutorial online "||","|"
-            //Array[][][] x = null;
+            String[] sep = new String[] { "||" };
             String[] mainSplit = events.Split(sep, StringSplitOptions.None);
             String[,] eventsSplit = new String[mainSplit.Length,2];
             for (int i = 0; i < mainSplit.Length; i++)
             {
-                String[] temp = mainSplit[i].Split('|');
-                eventsSplit[i,0] = temp[0];
-                eventsSplit[i,1] = temp[1];
-                Console.WriteLine(eventsSplit[i, 0]); //DEBUG
-                Console.WriteLine(eventsSplit[i, 1]); //DEBUG
+                if(!mainSplit[i].Contains("|")){
+                    eventsSplit[i, 0] =  mainSplit[i];
+                    Console.WriteLine(eventsSplit[i, 0]); //DEBUG
+                }
+                else{
+                    String[] temp = mainSplit[i].Split('|');
+                    eventsSplit[i,0] = temp[0];
+                    eventsSplit[i,1] = temp[1];
+                    Console.WriteLine(eventsSplit[i, 0]); //DEBUG
+                    Console.WriteLine(eventsSplit[i, 1]); //DEBUG
+
+                }
+            }
+            Dictionary<string, int> eventDict = new Dictionary<string, int>();
+            for (int i = 0; i < eventsSplit.GetLength(0); i++){
+                if(eventsSplit[i, 1] == ""){
+                    String[] temp = eventsSplit[i,0].Split(':');
+                    Console.WriteLine(temp[0]); //DEBUG
+                    Console.WriteLine(temp[1]); //DEBUG
+                    eventDict.Add(temp[0],Convert.ToInt32(temp[1]));
+                }
+                else{
+                    String[] temp = eventsSplit[i,0].Split(':');
+                    eventDict.Add(temp[0],Convert.ToInt32(temp[1]));
+                    Console.WriteLine(temp[0]); //DEBUG
+                    Console.WriteLine(temp[1]); //DEBUG
+                    temp = eventsSplit[i,1].Split(':');
+                    Console.WriteLine(temp[0]); //DEBUG
+                    Console.WriteLine(temp[1]); //DEBUG
+                    eventDict.Add(temp[0],Convert.ToInt32(temp[1]));
+
+                }
             }
             return null; //return parsed dict: eventsSplit
         }
